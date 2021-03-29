bf4e977c9590f90a8e0d980c734576e6ca0eec85
zgyr <0112d759@gmail.com>
2021-03-29 18:36:49 +0600

    added Layer

--- statcell_torch.py
+++ statcell_torch.py
@@ -1,5 +1,6 @@
 import torch
 
+
 class Cell:
     def __init__(self, inputs, size=32):
         self.inputs = inputs
@@ -26,3 +27,18 @@ class Cell:
                torch.sqrt(self.var + 0.001) * self.size * 0.5)
         return data
 
+
+class Layer:
+    def __init__(self, inputs, outputs, cell_size=32):
+        self.inputs = inputs
+        self.outputs = outputs or inputs
+        self.cells = []
+        for c in range(outputs):
+            self.cells.append(Cell(inputs, cell_size))
+    
+    def step(self, data):
+        result = []
+        for cell in self.cells:
+            result.append(torch.sum(cell.step(data)))
+        return torch.tensor(result).float()
+            
